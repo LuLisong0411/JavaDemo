7d6fafa3496fa60a0ce0569ba4aef0b62e42638f
LuLisong0411 <951170572@qq.com>
2018-10-17 23:27:08 +0800

    数据的转换

---
+++ JavaDemo/src/basic/DateChange.java
@@ -0,0 +1,19 @@
+/*
+ * 
+ * 数据类型转换
+ * 
+ * */
+package basic;
+
+public class DateChange {
+
+	public static void main(String[] args) {
+		int i = 333889890;
+		System.out.println("int i = "+i);
+		double j = i;
+		System.out.println("double j = "+j);
+		float k = i;
+		System.out.println("float k = "+k);
+	}
+
+}
