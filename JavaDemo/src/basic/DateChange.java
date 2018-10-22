95ab8be18a5c4a95bfcdeb349f9142a14330550f
LuLisong0411 <951170572@qq.com>
2018-10-22 23:07:46 +0800

    1.前几个代码注释的添加；2.类型之间的低到高自动转换和高到低强制转换。

--- JavaDemo/src/basic/DateChange.java
+++ JavaDemo/src/basic/DateChange.java
@@ -8,12 +8,12 @@ package basic;
 public class DateChange {
 
 	public static void main(String[] args) {
-		int i = 333889890;
-		System.out.println("int i = "+i);
-		double j = i;
-		System.out.println("double j = "+j);
-		float k = i;
-		System.out.println("float k = "+k);
+		int i = 333889890;							//声明一个整型变量
+		System.out.println("int i = "+i);			//输出整型变量i
+		double j = i;								//声明一个双精度型变量d，并将i值赋给d
+		System.out.println("double j = "+j);		//输出双精度型变量
+		float k = i;								//声明一个浮点型变量f，并将i的值赋给f
+		System.out.println("float k = "+k);			//输出浮点型变量
 	}
 
 }
